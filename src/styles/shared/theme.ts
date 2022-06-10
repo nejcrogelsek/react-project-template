554d95ca2c4a4e10d7911179f4b08be1072f0577
Nejc <nejcrogelsek0@gmail.com>
2022-06-10 23:36:23 +0200

    fix theme

--- src/styles/shared/theme.ts
+++ src/styles/shared/theme.ts
@@ -2,15 +2,6 @@ const theme = {
   colors: {
     adm_primary: '#39ad31',
     adm_primary_hover: '#30932a',
-    adm_secondary: '#1e242a',
-    adm_error: '#dc3545',
-    adm_error_hover: '#be313f',
-    adm_blue: '#0066d2',
-    adm_blue_hover: '#0251a5',
-    adm_orange: '#FFBC42',
-    adm_orange_hover: '#FFAA0F',
-    adm_disabled: '#DFE5EA',
-    adm_border: '#d1d1d1',
   },
   screens: {
     xs: '@media(max-width: 576px)',
